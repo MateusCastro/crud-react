2bc013416afc3bf3e2504fb4c95a872f3a8e8c4f
Mateus Castro <mateus.castro.domingos@gmail.com>
2018-05-15 17:27:30 -0300

    Comentários adicionados

--- src/App.js
+++ src/App.js
@@ -4,65 +4,65 @@ import './App.css';
 class App extends Component {
 
   constructor(props){
-    super(props)
+    super(props) // Método para atender o construtor da classe pai
     this.state = {
-      titulo: "CRUD simples com React",
-      acao: 0,
-      indice: "",
+      titulo: "CRUD simples com React", // Título da página
+      acao: 0, // Definirá se a ação é de inserir ou Alterar. 0 para inserir e 1 para alterar
+      indice: "", // Posição do dado no array caso a ação seja de Alterar
       dados: []
     }
   }
 
   salvar = (e) => {
     e.preventDefault()
-    let dados = this.state.dados
-    let nome = this.refs.nome.value
-    let cargo = this.refs.cargo.value
+    let dados = this.state.dados // Recebe os dados
+    let nome = this.refs.nome.value // Recebe as informações do input nome
+    let cargo = this.refs.cargo.value // Recebe as informações do input Cargo
 
-    if(this.state.acao == 0){
-      let dado = {
+    if(this.state.acao == 0){ // Caso a ação seja de inserir
+      let dado = { // variável com modelo do dado
         nome, cargo
       }
-      dados.push(dado)
-    }else{
-      let indice = this.state.indice
-      dados[indice].nome = nome
-      dados[indice].cargo = cargo
+      dados.push(dado) // inserindo na ultima posição da variável que recebe os dados
+    }else{ // caso a ação seja de alterar
+      let indice = this.state.indice // Recebe a posição do dado no array
+      dados[indice].nome = nome // Altera o campo nome
+      dados[indice].cargo = cargo // Altera o campo cargo
     }
 
-    this.setState({
-      dados: dados,
-      acao: 0
+    this.setState({ // Método para setar a variável state
+      dados: dados, // recebe os dados atualizados
+      acao: 0 // retorna o campo acao para o valor default
     })
 
-    this.refs.formulario.reset()
+    this.refs.formulario.reset() // reseta o formulário
   }
 
   remover = (i) => {
-    let dados = this.state.dados
-    dados.splice(i, 1)
-    this.setState({
-      dados: dados
+    let dados = this.state.dados // recebe os dados
+    dados.splice(i, 1) // Motodo para remover a posição no array
+    this.setState({ // Método para setar a variável state
+      dados: dados // Altera o campo dados
     })
-    this.refs.formulario.reset()
+    this.refs.formulario.reset() // reseta o formulário
   }
 
   editar = (i) => {
-    let dados = this.state.dados[i]
-    this.refs.nome.value = dados.nome
-    this.refs.cargo.value = dados.cargo
+    let dados = this.state.dados[i] // recebe dados 
+    this.refs.nome.value = dados.nome // Preenche o campo Nome com o valor do dado que quer editar
+    this.refs.cargo.value = dados.cargo // Preenche o campo cargo com o valor do dado que quer editar
 
-    this.setState({
-      acao: 1,
-      indice: i
+    this.setState({ // Método para setar state
+      acao: 1, // Altera a ação pra editar
+      indice: i // altera para o indice do dado
     })
 
-    this.refs.nome.focus()
+    this.refs.nome.focus() // foca o cursor no campo nome
     
   }
 
   render() {
-    let dados = this.state.dados
+    let dados = this.state.dados // recebe dados
     return (
       <div className="App">
         <h1>{this.state.titulo}</h1>
@@ -74,7 +74,7 @@ class App extends Component {
         <hr></hr>
         <pre>
           {
-            dados.map((item, i) => 
+            dados.map((item, i) => // percorre a variável dados que recebeu o Dados de state
               <li>
                 <label>{item.nome},  {item.cargo}</label>
                 <button onClick={()=>this.remover(i)}>remover</button>
@@ -88,4 +88,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App; // exporta a classe app
